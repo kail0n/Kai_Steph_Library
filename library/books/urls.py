e3a6f38f1acf072efba69565ec7f0de863379fad
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-21 16:58:37 +0000

    add users app and signup

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from .views import homepage, show
+from users import views as user_views
 
 urlpatterns = [
 
     path("", homepage),
 
     # path("", views.home, name='books-home'),
-    path("books/<int:book_id>/", show, name='books-show')
+    path("books/<int:book_id>/", show, name='books-show'),
+
+    path('signup/', user_views.signup, name='signup')
 ]
